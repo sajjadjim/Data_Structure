e66dd87204cb9c07a1a22761eda39f6fe782ed74
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-07-25 11:39:30 +0600

    Add files via upload

---
+++ 3 Data part.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+ struct node {
+ int x ;
+ float y;
+ char z;
+ struct node *next;  // self refefarencial pointer
+ };
+ typedef struct node head;
+ int main()
+{
+  head *node1 , * node2;
+  node1 =(head*)malloc(sizeof(head));   // Memory Address create
+  node2 =(head*)malloc(sizeof(head));   // Memory Address create
+  node1-> x =6;
+  node1->y =64.99;
+  node1->z = 'Aim';
+  node1->next=node2;
+
+  node2-> x = 9;
+  node2->y =3.19;
+  node2->z = 'Blim';
+  node2->next=NULL;
+
+    cout <<"Print value "<< node1->x << endl;
+    cout <<"Print value "<< node1->y << endl;
+    cout <<"Print value "<< node1->z << endl<<endl;
+
+    cout <<"Print value "<< node2->x << endl;
+    cout <<"Print value "<< node2->y << endl;
+    cout <<"Print value "<< node2->z << endl;
+
+ return 0;
+}
