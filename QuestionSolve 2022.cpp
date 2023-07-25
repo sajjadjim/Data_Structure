56becfb1b1ccc3395582202a903fdf7d2797e03b
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-07-25 12:24:59 +0600

    Add files via upload

---
+++ QuestionSolve 2022.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+ struct node {
+ int x ;
+ float y;
+ char z;
+ struct node *next;  // self refarencial pointer
+ };
+ typedef struct node head;
+ int main()
+{
+  head *baby , *fish ,*cat;
+  baby =(head*)malloc(sizeof(head));   // Memory Address create
+  fish =(head*)malloc(sizeof(head));   // Memory Address create
+  cat =(head*)malloc(sizeof(head));   // Memory Address create
+
+  baby->x =7; baby->y =1.5; baby->z = 'A';
+  baby->next=fish;
+
+  fish->x = 10; fish->y =3.5; fish->z = 'B';
+  fish->next=cat;
+
+  cat->x = 13; cat->y =5.5; cat->z = 'C';
+  cat->next=NULL;
+
+    cout <<"Print value "<< baby->x << endl;
+    cout <<"Print value "<< baby->y << endl;
+    cout <<"Print value "<< baby->z << endl<<endl;
+
+    cout <<"Print value "<< fish->x << endl;
+    cout <<"Print value "<< fish->y << endl;
+    cout <<"Print value "<< fish->z << endl<<endl;
+
+    cout <<"Print value "<< cat->x << endl;
+    cout <<"Print value "<< cat->y << endl;
+    cout <<"Print value "<< cat->z << endl;
+
+ return 0;
+}
+
---
+++ Spring 2023.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct node{
+struct node *prev;
+char data;
+struct node *next;
+};
+typedef struct node head;
+int main()
+{
+    node *n1 , *n2 , *n3 ,*n4;
+
+    n1 =(head*)malloc(sizeof(n1));
+    n2 =(head*)malloc(sizeof(n2));
+    n3 =(head*)malloc(sizeof(n3));
+    n4 =(head*)malloc(sizeof(n4));
+
+    n1->data ='A';
+    n1->next = n2;
+
+    n2->data ='B';
+    n2->next =n3;
+    n3->prev =n2;
+
+    n3->data ='B';
+    n3->next =n4;
+    n3->prev =n2;
+
+    n4->data ='D';
+    n4->next =NULL;
+    n4->prev =n3;
+
+    cout << n1->data << endl;
+}
