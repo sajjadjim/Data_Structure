22e0ce2621023c4d7ba04a60bfd04e58b56cd7a0
S.M. SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2023-08-07 01:17:21 +0600

    Update 3 Data part.cpp

--- 3 Data part.cpp
+++ 3 Data part.cpp
@@ -5,7 +5,7 @@ using namespace std;
  int x ;
  float y;
  char z;
- struct node *next;  // self refefarencial pointer
+ struct node *next;  // self refefarencial pointer creation
  };
  typedef struct node head;
  int main()
